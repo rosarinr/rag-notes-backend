6a5feda68013991dfb56dc456667abd12b461100
Rosarin <zenpinn@gmail.com>
2025-09-01 16:29:57 +0700

    update cors

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
-    "https://rag-notes.vercel.app",
+    "https://rag-notes-frontend-two.vercel.app",
   ], // frontend domain
   credentials: true, // ✅ allow cookies to be sent
 };

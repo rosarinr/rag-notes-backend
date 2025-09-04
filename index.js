ee28621e78586cdb4e4e677f28bc6d9b023817a6
Rosarin <zenpinn@gmail.com>
2025-09-04 15:06:44 +0700

    add 0.0.0.0 in port

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ const PORT = process.env.PORT || 3000;
   try {
     await connectMongo();
     await connectTurso();
-    app.listen(PORT, () => {
+    app.listen(PORT,"0.0.0.0", () => {
       console.log(`Server listening on port ${PORT}`);
     });
   } catch (err) {
